94f12f58ebd8b1e0fe2ac1834152aa15e18197cd
sethc <sethc@Cudlecakes-Lap-Warmer>
2017-03-08 23:08:39 -0600

    Chapter 1 Labs Commit

---
+++ src/addNumsLauncher.java
@@ -0,0 +1,15 @@
+
+public class addNumsLauncher {
+
+	public static void main(String[] args) {
+		addNums number1 = new addNums(2,3);
+		addNums number2 = new addNums(7,10);
+		addNums number3 = new addNums(5,4);
+		addNums number4 = new addNums(19,3);
+		addNums number5 = new addNums(6,13);
+
+
+
+	}
+
+}
